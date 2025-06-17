a81b33c97539170bc99d56e8cc59e0d66f214956
Hungry-Dolphin <spoekie99@gmail.com>
2025-06-17 12:31:09 +0200

    fleshed out web app framework and added (flawed) basic model

---
+++ Drop-Recovery-Predictor/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+# app.config.from_object('config')
+
+@app.route('/')
+def home():
+    return render_template('pages/landing_page.html')
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    #db_session.rollback()
+    return render_template('errors/500.html'), 500
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
+
+if __name__ == '__main__':
+    app.run()
+
